aa29fe2e737e50ba12c024f8f7daa5e97358f9ed
Leandro Nunes dos Santos <leandronunes@gmail.com>
2013-07-06 08:28:32 -0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Partidados::Application.config.secret_token = 'bd931c549d3b8b203521f95f83bb749450ef3388b87b8823f0e0ee33531240a954c20aea67e97445aceb5055358dceebbb14e3e1b7625cefdea0a222d3d33bc5'
